906f127e74d27c21cd99364155b9dde7bfb1007d
bradgeigermanager-sketch <brad.geiger.manager@gmail.com>
2026-06-17 22:30:25 -0600

    Add TemplateEditor component with placeholder content

---
+++ ui/src/components/TemplateEditor.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TemplateEditor: React.FC = () => {
+  return (
+    <div>
+      <h2>Template Editor</h2>
+      <p>Drag and drop editor placeholder. Implement with react-beautiful-dnd.</p>
+    </div>
+  );
+};
+
+export default TemplateEditor;
